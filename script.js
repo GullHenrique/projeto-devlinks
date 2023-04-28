dba29b7e24ae7c02d6d3ea59b35f7e4603dfb6b3
Gustavo Henrique <gustavohdcouto@gmail.com>
2023-04-27 23:00:28 -0300

    first commit

--- script.js
+++ script.js
@@ -2,21 +2,12 @@ function toggleMode() {
   const html = document.documentElement
   html.classList.toggle("light")
 
-  //pegar tag img
   const img = document.querySelector("#profile img")
-  //substituir a img
   if (html.classList.contains("light")) {
-    //se tiver light mode adicionar a image light
     img.setAttribute("scr", "./assets/avatar-light.png")
     img.setAttribute("alt", "Foto de Perfil Gu Henrique Escura")
   } else {
-    //se tiver sem light mode manter a img normal
     img.setAttribute("scr", "./assets/avatar.png")
     img.setAttribute("alt", "Foto de Perfil Gu Henrique")
   }
-  // if (html.classList.contains("light")) {
-  //   html.classList.remove("light")
-  // } else {
-  //   html.classList.add("light")
-  // }
 }
